cff24fb7898a33be925066779998625031ae8268
Luke Kim <80174+lukekim@users.noreply.github.com>
2022-04-08 08:37:02 +0900

    Return FlightStreamReader (#7)

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# spicedata-py
+# spice-py
 
 Spice client for Python.
-
 
 ## Installation
 
@@ -17,7 +16,6 @@ From code:
 pip install -e .
 ```
 
-
 ## Usage
 
 ```python
@@ -25,10 +23,11 @@ from spicedata import Client
 
 client = Client('API_KEY')
 data = client.query('SELECT * FROM eth.recent_blocks LIMIT 10;')
+pd = data.read_pandas()
 ```
 
 Querying data is done through a `Client` object that initialize the connection with Spice endpoint. `Client` have the following arguments:
 
-* **api_key** (string, required): API key to authenticate with the endpoint.
-* **url** (string, optional): URL of the endpoint to use (default: grpc+tls://flight.spiceai.io)
-* **tls_root_cert** (Path or string, optional): Path to the tls certificate to use for the secure connection (ommit for automatic detection)
+- **api_key** (string, required): API key to authenticate with the endpoint.
+- **url** (string, optional): URL of the endpoint to use (default: grpc+tls://flight.spiceai.io)
+- **tls_root_cert** (Path or string, optional): Path to the tls certificate to use for the secure connection (ommit for automatic detection)
--- spicedata/_client.py
+++ spicedata/_client.py
@@ -10,17 +10,28 @@ from pyarrow import flight
 
 
 class Client:
-    def __init__(self, api_key: str, url: str = 'grpc+tls://flight.spiceai.io', tls_root_cert: Union[str, Path] = None):
-        cert_path = Path(Path.cwd().absolute().anchor) / 'usr' / 'share' / 'grpc' / 'roots.pem'
+    def __init__(
+        self,
+        api_key: str,
+        url: str = "grpc+tls://flight.spiceai.io",
+        tls_root_cert: Union[str, Path] = None,
+    ):
+        cert_path = (
+            Path(Path.cwd().absolute().anchor) / "usr" / "share" / "grpc" / "roots.pem"
+        )
         if tls_root_cert is not None:
-            tls_root_cert = tls_root_cert if isinstance(tls_root_cert, Path) else Path(tls_root_cert)
+            tls_root_cert = (
+                tls_root_cert
+                if isinstance(tls_root_cert, Path)
+                else Path(tls_root_cert)
+            )
         if not cert_path.exists():
-            env_name = 'GRPC_DEFAULT_SSL_ROOTS_FILE_PATH'
+            env_name = "GRPC_DEFAULT_SSL_ROOTS_FILE_PATH"
             if env_name not in os.environ or not Path(os.environ[env_name]).exists():
-                temp_cert_path = Path(tempfile.gettempdir()) / 'roots.pem'
+                temp_cert_path = Path(tempfile.gettempdir()) / "roots.pem"
                 if not Path(temp_cert_path).exists():
                     ssl._create_default_https_context = ssl._create_unverified_context
-                    urlretrieve('https://pki.google.com/roots.pem', str(temp_cert_path))
+                    urlretrieve("https://pki.google.com/roots.pem", str(temp_cert_path))
                 os.environ[env_name] = str(temp_cert_path)
 
         self._api_key = api_key
@@ -29,17 +40,22 @@ class Client:
         self._authenticate()
 
     def _authenticate(self):
-        headers = [self._flight_client.authenticate_basic_token('', self._api_key)]
+        headers = [self._flight_client.authenticate_basic_token("", self._api_key)]
         self._flight_options = flight.FlightCallOptions(headers=headers)
 
-    def query(self, query: str) -> pd.DataFrame:
+    def query(self, query: str) -> flight.FlightStreamReader:
         flight_info = self._flight_client.get_flight_info(
-            flight.FlightDescriptor.for_command(query), self._flight_options)
+            flight.FlightDescriptor.for_command(query), self._flight_options
+        )
         try:
-            reader = self._flight_client.do_get(flight_info.endpoints[0].ticket, self._flight_options)
+            reader = self._flight_client.do_get(
+                flight_info.endpoints[0].ticket, self._flight_options
+            )
         except flight.FlightUnauthenticatedError:
-            print('Re authenticating')
+            print("Re authenticating")
             self._authenticate()
-            reader = self._flight_client.do_get(flight_info.endpoints[0].ticket, self._flight_options)
+            reader = self._flight_client.do_get(
+                flight_info.endpoints[0].ticket, self._flight_options
+            )
 
-        return reader.read_pandas()
+        return reader
