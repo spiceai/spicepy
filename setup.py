a0796e26947b4ea51308ab48d3d51e033d79c5a4
Luke Kim <80174+lukekim@users.noreply.github.com>
2022-04-08 10:38:21 +0900

    Set min Python to 3.7 and update metadata 3.7 is required for Kaggle

--- setup.py
+++ setup.py
@@ -9,19 +9,20 @@ import setuptools
 
 def setup_package():
     setuptools.setup(
-        name='spicedata',
-        version='0.1.0',
-        maintainer='SpiceAI',
-        author='Corentin Risselin',
-        author_email='corentin@spiceai.io',
-        url='https://github.com/spicehq/spicedata-py',
-        description='Simple Apache Arrow Flight endpoint access',
-        license='Apache',
-        packages=['spicedata'],
-        install_requires=['pyarrow', 'pandas'],
-        python_requires='>=3.8'
+        name="spicedata",
+        version="0.1.0",
+        maintainer="Spice AI, Inc.",
+        maintainer_email="webmaster@spiceai.io"
+        author="Corentin Risselin",
+        author_email="corentin@spiceai.io",
+        url="https://github.com/spicehq/spicedata-py",
+        description="Spice.xyz Python SDK - data and AI infrastructure for web3.",
+        license="Apache 2.0",
+        packages=["spicedata"],
+        install_requires=["pyarrow", "pandas"],
+        python_requires=">=3.7",
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup_package()
