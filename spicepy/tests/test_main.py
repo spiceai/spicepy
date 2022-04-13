fc0bdbc89f3fb73f4773988992e0c1bb315451b3
corentin <corentin@spice.ai>
2022-04-13 13:59:09 +0200

    Fix for lint

--- spicepy/tests/test_main.py
+++ spicepy/tests/test_main.py
@@ -4,5 +4,5 @@ from spicepy import Client
 def test_recent_blocks():
     client = Client("3534|0a05e0808ff647ea98a656efab3f7e30")
     data = client.query("SELECT * FROM eth.recent_blocks LIMIT 10;")
-    pd = data.read_pandas()
-    assert len(pd) == 10
+    pandas_data = data.read_pandas()
+    assert len(pandas_data) == 10
