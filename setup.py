e6f14e9361250f173ae4a7dd2634c887cbca765a
Luke Kim <80174+lukekim@users.noreply.github.com>
2022-05-31 15:19:39 +0900

    Tweaks

--- setup.py
+++ setup.py
@@ -12,15 +12,15 @@ def setup_package():
         name="spicepy",
         version="0.1.0",
         maintainer="Spice AI, Inc.",
-        maintainer_email="webmaster@spiceai.io",
-        author="Corentin Risselin",
-        author_email="corentin@spiceai.io",
+        maintainer_email="webmaster@spice.ai",
+        author="Spice AI, Inc.",
+        author_email="webmaster@spice.ai",
         url="https://github.com/spicehq/spice-py",
         description="Spice.xyz client library - data and AI infrastructure for web3.",
         license="Apache 2.0",
         packages=["spicepy"],
         install_requires=["pyarrow", "pandas", "web3>=6.0.0b2"],
-        python_requires=">=3.7",
+        python_requires=">=3.8",
     )
 
 
